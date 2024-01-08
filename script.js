771d310cc166a30abad2373fa70dd04c9dc919d4
MohiniMehta <mohinimehta12@gmail.com>
2024-01-08 21:55:00 +0530

    hided the source

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ document.addEventListener('DOMContentLoaded', function () {
     var confetti = new ConfettiGenerator(confettiSettings);
     confetti.render();
 });
+
+document.oncontextmenu = ()=>{
+    alert("Dont try right click")
+    return false
+}
+document.onkeydown=e=>{
+    if(e.key=="F12"){
+        alert("Dont try to inspect element")
+        return false
+    }
+    if(e.ctrlKey && e.key=="u"){
+        alert("Dont try to view page")
+        return false
+    }
+    if(e.ctrlKey && e.key=="c"){
+        alert("Dont try to copy page element")
+        return false
+    }
+}
